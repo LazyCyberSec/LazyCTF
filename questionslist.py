78af62cc0e65565644c663122757204f06f1e302
LazySec <51275510+LazyCyberSec@users.noreply.github.com>
2023-10-28 21:47:38 -0700

    Add files via upload

---
+++ questionslist.py
@@ -0,0 +1,12 @@
+questionslist = {
+    "question1": {
+        "description": "Solve this puzzle: puzzle.txt",
+        "solution": "CTF{puzzle1}",
+        "points": 10
+    },
+    "question2": {
+        "description": "Decrypt this message: encrypted_message = '1234567890'",
+        "solution": "CTF{decrypted2}",
+        "points": 20
+    },
+}
